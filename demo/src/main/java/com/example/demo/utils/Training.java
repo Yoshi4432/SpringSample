32f09386e8447621d0e5728a61481f935458359f
Yoshi4432 <perfect.yoshi@gmail.com>
2022-02-15 16:22:27 +0900

    モード変更の仕組みを追加2

---
+++ demo/src/main/java/com/example/demo/utils/Training.java
@@ -0,0 +1,20 @@
+package com.example.demo.utils;
+
+/**
+ * トレーニング用の定数を定義する.
+ * 
+ * @author perfect.yoshi@gmail.com
+ *
+ */
+public class Training {
+
+	/**
+	 * 動作モード
+	 * 
+	 * @author perfect.yoshi@gmail.com
+	 *
+	 */
+	public enum Mode {
+		ANGULAR, REACT, THYMELEAF, VUE;
+	}
+}
