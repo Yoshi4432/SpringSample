6629703dc84934536e35fbee2052272d1f55e23f
Yoshi4432 <perfect.yoshi@gmail.com>
2022-02-15 16:20:34 +0900

    モード変更の仕組みを準備.

--- demo/src/main/java/com/example/demo/controllers/impl/IndexController.java
+++ demo/src/main/java/com/example/demo/controllers/impl/IndexController.java
@@ -1,25 +1,32 @@
 package com.example.demo.controllers.impl;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.controllers.BaseController;
-import com.example.demo.controllers.dtos.BaseControllerDto;
-import com.example.demo.forms.BaseForm;
+import com.example.demo.utils.Training;
 
+/**
+ * 初期表示コントローラ
+ * 
+ * @author perfect.yoshi@gmail.com
+ *
+ */
 @Controller
 public class IndexController extends BaseController {
+
+	@Value("${training.mode}")
+	private String mode;
 
 	@GetMapping({ "/", "/index" })
 	public String index() {
 		// パラメータ設定
-		String page = addAttributesForThymeleaf(null, null);
-		return page;
+		if (Training.Mode.THYMELEAF.name().toString().equals(mode)) {
+			// タイムリーフ
+			return dirThymeleaf + "/index";
+		} else {
+			return "/mock/index";
+		}
 	}
-
-	@Override
-	protected String addAttributesForThymeleaf(BaseForm form, BaseControllerDto dto) {
-		return dirThymeleaf + "/index";
-	}
-
 }
